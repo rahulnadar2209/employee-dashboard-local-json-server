e1a47f470e5bbc7493b06336d48389784bc52faa
rahulnadar2209 <rahulnadar2209@gmail.com>
2021-06-28 23:27:22 +0530

    2 files modified

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-dashboard',
@@ -6,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-dashboard.component.css']
 })
 export class EmployeeDashboardComponent implements OnInit {
-
-  constructor() { }
+  formvalue !:FormGroup;
+  constructor(private formbuilder:FormBuilder) { }
 
   ngOnInit() {
   }
