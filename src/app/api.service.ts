852f6d530e610a1ce8e168ee5ad80a0edfa00756
rahulnadar2209 <rahulnadar2209@gmail.com>
2021-06-29 00:36:06 +0530

    3 files modified, 1 added

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,10 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ApiService {
-
+  public url="http://localhost:3000/user"
   constructor(private http:HttpClient) { }
-  postemployee(data:any){}
+  postemployee(data:any){
+    return this.http.post<any>(this.url,data)
+                .pipe(map((res:any)=>{
+                  return res;
+                }))
+  }
+
+  getemployee(){
+    return this.http.get<any>(this.url)
+                .pipe(map((res:any)=>{
+                  return res;
+                }))
+  }
+
+  updateemployee(data:any,id:number){
+    return this.http.put<any>(this.url+'/'+id,data)
+                .pipe(map((res:any)=>{
+                  return res;
+                }))
+  }
+  deleteemployee(data:any,id:number){
+    return this.http.delete<any>(this.url+'/'+id)
+                .pipe(map((res:any)=>{
+                  return res;
+                }))
+  }
 
 }
--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../api.service';
+import { EmployeeModel } from './employee-model';
 
 @Component({
   selector: 'app-employee-dashboard',
@@ -7,17 +9,36 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./employee-dashboard.component.css']
 })
 export class EmployeeDashboardComponent implements OnInit {
-  formvalue !:FormGroup;
-  constructor(private formbuilder:FormBuilder) { }
+  formvalue!: FormGroup;
+  employeemodelObj: EmployeeModel = new EmployeeModel();
+
+  constructor(private formbuilder: FormBuilder, private api: ApiService) {}
 
   ngOnInit() {
     this.formvalue = this.formbuilder.group({
-      firstname:[''],
-      lastname:[''],
-      email:[''],
-      phoneno:[''],
-      salary:['']
-    })
+      firstname: [''],
+      lastname: [''],
+      email: [''],
+      phoneno: [''],
+      salary: ['']
+    });
   }
+  postemployeedetails() {
+    this.employeemodelObj.firstname = this.formvalue.value.firstname;
+    this.employeemodelObj.lastname = this.formvalue.value.lastname;
+    this.employeemodelObj.email = this.formvalue.value.email;
+    this.employeemodelObj.mobile = this.formvalue.value.phoneno;
+    this.employeemodelObj.salary = this.formvalue.value.salary;
 
+    this.api.postemployee(this.employeemodelObj).subscribe(
+      res => {
+        console.log(res);
+        alert('Employee added successfully!!');
+        this.formvalue.reset();
+      },
+      err => {
+        alert('something went wrong!');
+      }
+    );
+  }
 }
